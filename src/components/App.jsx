f38d70bf989189b9289877195cdec79aeb3a9781
Vyacheslav Badlaev <127213166+SlavaBadlaev@users.noreply.github.com>
2023-06-07 18:44:37 +0300

    Add simple App,Navbar,MainPage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './Pages/MainPage';
+import NavBar from './UI/NavBar';
 
-export default function App({ hello }) {
-  return <div>Hello, {hello}</div>;
+export default function App() {
+  return (
+    <>
+    <NavBar />
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+      </Routes>
+    </>
+  )
 }

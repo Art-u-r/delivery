786ce70a4e90bb06956f71a5eefd501ec7c7227d
Vyacheslav Badlaev <127213166+SlavaBadlaev@users.noreply.github.com>
2023-06-08 15:28:10 +0300

    Fix routes and App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import NavBar from './ui/NavBar';
-import CustomerRegPage from './pages/auth/customer/CustomerRegPage';
-import MainPage from './pages/main/MainPage';
-import LoginPage from './pages/login/LoginPage';
+import NavBar from './UI/NavBar';
+import CustomerRegPage from './Pages/auth/customer/CustomerRegPage';
+import MainPage from './Pages/main/MainPage';
+import LoginPage from './Pages/login/LoginPage';
 
 export default function App({ orders, user }) {
   return (
@@ -11,7 +11,6 @@ export default function App({ orders, user }) {
       <NavBar user={user} />
       <Routes>
         <Route path='/' element={<MainPage orders={orders} />} />
-        <Route path="/" element={<MainPage />} />
         <Route path="/auth/join" element={<CustomerRegPage />} />
         <Route path="/auth/login" element={<LoginPage />} />
       </Routes>
--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { Order } from '../../db/models'
+import { Order } from '../../db/models';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   const orders = await Order.findAll();
   const initState = { orders };
-  res.render('Layout', initState)
+  res.render('Layout', initState);
+});
 
 router.get('/account', (req, res) => {
   const initState = {};
